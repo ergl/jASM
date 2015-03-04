97a8671ecb596bfdd7bf3a7fe8ee6421793cc159
ergl <ergl@users.noreply.github.com>
2015-03-04 19:51:13 +0100

    Added hello world example asm file

---
+++ examples/hello-world.asm
@@ -0,0 +1,28 @@
+; This program will print 'Hello, World!' to the specified out option (use the -o option)
+push 72
+out
+push 101
+out
+push 108
+push 108
+out
+out
+push 111
+out
+push 44
+out
+push 32
+out
+push 87
+out
+push 111
+out
+push 114
+out
+push 108
+out
+push 100
+out
+push 33
+out
+halt
